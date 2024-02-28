84899007d3bbc2e17425c8cdc482c8c67f0d5dc8
Kiranraj27 <kiranraj586@gmail.com>
2024-02-28 16:08:10 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kiranraj27/gosql
+
+go 1.21.1
+
+require github.com/go-sql-driver/mysql v1.7.1
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	cfg := mysql.Config{
+		User:   "root",
+		Passwd: "sample",
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+}
